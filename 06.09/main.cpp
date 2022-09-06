37c2af0d3ee8cf0c9f60b8fdbcbb8fafc41a8006
Kris <krischerkezyan86@gmail.com>
2022-09-06 14:41:35 +0300

    Save student

--- 06.09/main.cpp
+++ 06.09/main.cpp
@@ -8,22 +8,16 @@ class Student
 public:
 	Student()
 	{
-
+		cout << "Hello student\n";
 	}
 	Student(string n, int a)
 	{
 		name = n;
 		age = a;
 	}
-
-	void Print()
-	{
-		cout << "X: " << x << "\nY: " << y << endl << endl;
-	}
 };
 int main()
 {
-	Point obj1;
-	Point obj2(1, 2);
-	obj2.Print();
+
+	
 }
